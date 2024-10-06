63828c79c51b881db16e56e04faffb8cd4ecfa9b
Carla <cast4730@mylaurier.ca>
2024-10-05 20:53:33 -0400

    fixed navbar implementation

--- angular-trading-ui/src/app/app.component.ts
+++ angular-trading-ui/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,DashboardComponent],
+  imports: [RouterOutlet,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
